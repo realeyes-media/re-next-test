e11d749d69472f9c7e88bd1a8386a688a9c3559f
Mark Grant <grant.mark@gmail.com>
2019-04-09 13:14:37 -0600

    close button now showing and working in mobile

--- hugo/themes/hugo-hero-theme/assets/js/scripts.js
+++ hugo/themes/hugo-hero-theme/assets/js/scripts.js
@@ -1,8 +1,11 @@
 var body = document.querySelector('body')
 var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
 var menuContainer = document.querySelector('#main-menu-mobile');
+const hambugerButton = document.getElementById('toggle-main-menu-mobile');
 
-menuTrigger.onclick = function() {
+menuTrigger.onclick = function () {
+    hambugerButton.style.position = "fixed";
+    hambugerButton.style.right = "1rem";
     menuContainer.classList.toggle('open');
     menuTrigger.classList.toggle('is-active')
     body.classList.toggle('lock-scroll')
